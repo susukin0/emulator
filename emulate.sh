5cc35577db9cfe94fbce47657b3394d1ed13fa0a
susukin0 <70662829+susukin0@users.noreply.github.com>
2025-01-11 21:50:37 +0300

    Update emulate.sh

--- emulate.sh
+++ emulate.sh
@@ -1,43 +1,30 @@
 #!/bin/bash
+
 set -e
 
-command -v javac > /dev/null 2>&1 || { echo >&2 "I require openjdk-11 but it's not installed. Install it. Aborting."; exit 1; }
+command -v javac > /dev/null 2>&1 || { echo >&2 "I require openjdk-17 but it's not installed. Install it. Aborting."; exit 1; }
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
 
-AVDMANAGER="/opt/commandline-tools/cmdline-tools/bin/avdmanager"
-EMULATOR="/opt/emulator/emulator"
-SDKMANAGER="/opt/commandline-tools/cmdline-tools/bin/sdkmanager"
+AVDMANAGER="$HOME/commandline-tools/cmdline-tools/bin/avdmanager"
+EMULATOR="$HOME/emulator/emulator"
+SDKMANAGER="$HOME/commandline-tools/cmdline-tools/bin/sdkmanager"
 
 AVD_NAME="my_avd"
 
-[[ -e /opt/commandline-tools.zip ]] || wget -O /opt/commandline-tools.zip 'https://dl.google.com/android/repository/commandlinetools-linux-9123335_latest.zip' && [[ -d /opt/commandline-tools ]] || unzip /opt/commandline-tools.zip -d /opt/commandline-tools/
+[[ -e $HOME/commandline-tools.zip ]] || wget -O $HOME/commandline-tools.zip 'https://dl.google.com/android/repository/commandlinetools-linux-9123335_latest.zip' && [[ -d $HOME/commandline-tools ]] || unzip $HOME/commandline-tools.zip -d $HOME/commandline-tools/
 
-yes | $SDKMANAGER --install "emulator" "platforms;android-30" "platform-tools" --sdk_root=/opt/
-yes | $SDKMANAGER "system-images;android-30;google_apis_playstore;x86_64" --sdk_root=/opt/
-yes | $SDKMANAGER --update --sdk_root=/opt/
+yes | $SDKMANAGER --install "emulator" "platforms;android-30" "platform-tools" --sdk_root=$HOME/
+yes | $SDKMANAGER "system-images;android-30;google_apis_playstore;x86_64" --sdk_root=$HOME/
+yes | $SDKMANAGER --update --sdk_root=$HOME/
 
-# Create AVD if it doesn't exist
-if [[ ! -d /opt/.android/avd/$AVD_NAME.avd ]]; then
-    echo 'no' | $AVDMANAGER create avd -n $AVD_NAME -k "system-images;android-30;google_apis_playstore;x86_64" -p "/opt/.android/avd/$AVD_NAME.avd/" --force
-fi
+# Comment these lines to keep avd data
+[[ -d $HOME/.android/avd/$AVD_NAME.avd ]] && rm -rfd $HOME/.android/avd/$AVD_NAME.avd/*
+echo 'no' | $AVDMANAGER create avd -n $AVD_NAME -k "system-images;android-30;google_apis_playstore;x86_64" -p "$HOME/.android/avd/$AVD_NAME.avd/" --force
 
-# Enable Play Store and hardware keyboard
-sed -i -e 's/PlayStore.enabled.*/PlayStore.enabled = yes/' /opt/.android/avd/$AVD_NAME.avd/config.ini
-sed -i -e 's/hw.keyboard.*/hw.keyboard = yes/' /opt/.android/avd/$AVD_NAME.avd/config.ini
-
-# Enable GPU acceleration
-echo "hw.gpu.enabled = yes" >> /opt/.android/avd/$AVD_NAME.avd/config.ini
-echo "hw.gpu.mode = host" >> /opt/.android/avd/$AVD_NAME.avd/config.ini
-
-# Allocate 4000MB of RAM
-echo "hw.ramSize=4096" >> /opt/.android/avd/$AVD_NAME.avd/config.ini
-
-# Set NVIDIA GPU /option
-echo "hw.dPad=no" >> /opt/.android/avd/$AVD_NAME.avd/config.ini
+[[ -d $HOME/.android/avd/$AVD_NAME.avd ]] && sed -i -e 's/PlayStore.enabled.*/PlayStore.enabled = yes/' $HOME/.android/avd/$AVD_NAME.avd/config.ini
+[[ -d $HOME/.android/avd/$AVD_NAME.avd ]] && sed -i -e 's/hw.keyboard.*/hw.keyboard = yes/' $HOME/.android/avd/$AVD_NAME.avd/config.ini
 
 echo "All done! Running the device..."
 
 $EMULATOR -avd $AVD_NAME
-
-exit
